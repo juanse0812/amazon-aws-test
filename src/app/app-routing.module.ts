e8cf36746f3d21613adab094dd4211eec52b5dad
Juan <jspulgarin93@misena.edu.co>
2021-06-16 20:42:42 -0500

    test amazon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HelloWordComponent } from './hello-word/hello-word.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HelloWordComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloWordComponent } from './hello-word/hello-word.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWordComponent
   ],
   imports: [
     BrowserModule,
